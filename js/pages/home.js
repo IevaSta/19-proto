e4c89178e7e4efb1c7ca01d379de10d2781877df
IevaSta <ievastaseviciu@gmail.com>
2023-03-28 20:00:18 +0300

    Landing home page structure

--- js/pages/home.js
+++ js/pages/home.js
@@ -2,3 +2,24 @@
 
 //EXECUTION
 console.log('home page TEST');
+
+/* header: start */
+/* header: end */
+
+/* home: start */
+/* home: end */
+
+/* strategic: start */
+/* strategic: end */
+
+/* engineering: start */
+/* engineering: end */
+
+/* visual: start */
+/* visual: end */
+
+/* contacts: start */
+/* contacts: end */
+
+/* footer: start */
+/* footer: end */
